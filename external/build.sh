d43d69033b8060345c24c21051a03e89c86307dd
mellic03 <mellic03@gmail.com>
2025-10-26 18:21:36 +1000

    ree

--- external/build.sh
+++ external/build.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 export CMAKE_POLICY_VERSION_MINIMUM=3.11
 thisdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-BuildType=$1
 
 buildmd()
 {
     cd $thisdir
 
-    local srcpath=submodule/$1
-    local buildpath=build/$1
+    local buildtype=$1
+    local srcpath=submodule/$2
+    local buildpath=build/$buildtype/$2
     local installpath=install
-    shift # Remove $1
+    shift
+    shift
 
     local defines=$@
+
     mkdir -p $buildpath
 
-    cmake -DCMAKE_BUILD_TYPE=$BuildType -DCMAKE_INSTALL_PREFIX=$installpath $defines \
+    cmake -DCMAKE_BUILD_TYPE=$buildtype -DCMAKE_INSTALL_PREFIX=$installpath $defines \
           -S $srcpath -B $buildpath
     cmake --build $buildpath  -j8
     cmake --install $buildpath
 }
 
-buildmd assimp -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=ON -DASSIMP_BUILD_ZLIB=ON
-buildmd glad
-buildmd glm -DBUILD_SHARED_LIBS=OFF -DGLM_BUILD_TESTS=OFF -DGLM_FORCE_INTRINSICS=ON
-buildmd SDL3 -DSDL_SHARED=ON -DSDL_STATIC=ON
+# buildmd $1 assimp -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=ON -DASSIMP_BUILD_ZLIB=ON
+# buildmd $1 glad
+# buildmd $1 glm -DBUILD_SHARED_LIBS=OFF -DGLM_BUILD_TESTS=OFF -DGLM_FORCE_INTRINSICS=ON
+buildmd $1 SDL3 -DSDL_SHARED=ON -DSDL_STATIC=ON
 
 temp=$thisdir/install/.lib
 actual=$thisdir/install/lib
